0ec31e3051b75c2779a20ea50218de6188ffc1fc
Tyler Cobian <tylercobian@169-231-157-28.wireless.ucsb.edu>
2020-02-19 10:12:46 -0800

    Annotation

--- Working/Example_Function/ghg.R
+++ Working/Example_Function/ghg.R
@@ -1,12 +1,12 @@
 ghg<- function(element, kg){
+ # What you will have to put in the function, ghg("elemnt", kg = ) 
+  if(element == "C"){return(5.077*kg)} # Conversionn from kgC to kgCO2e
   
-  if(element == "C"){return(5.077*kg)} 
+  else if(element == "N2O"){return(624.643*kg)} # Conversion from kgN to KkgCO2e
   
-  else if(element == "N2O"){return(624.643*kg)}
+  else if (element == "CH4"){return(37.333*kg)} # Conversion from kgC to kgCO2e
   
-  else if (element == "CH4"){return(37.333*kg)}
-  
-  else {return("Can only convert CH4, N2O, and C, in units of kg to units of kgCO2e. CH4 is measured in units of kgC, N2O in units of kgN, and C in units of kgC")}
+  else {return("Can only convert CH4, N2O, and C, in units of kg to units of kgCO2e. CH4 is measured in units of kgC, N2O in units of kgN, and C in units of kgC")} # Error checking fro anything that is not C, CH4, or N2O
 }
 
 
