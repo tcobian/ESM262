ca997f22d7533218e27573fd32adf38ea355d4de
Tyler Cobian <tylercobian@169-231-157-28.wireless.ucsb.edu>
2020-02-19 09:43:14 -0800

    Make into a .R document

---
+++ Working/Example_Function/ghg.R
@@ -0,0 +1,10 @@
+ghg<- function(element, kg){
+  
+  if(element == "C"){return(5.077*kg)} 
+  
+  else if(element == "N2O"){return(624.643*kg)}
+  
+  else if (element == "CH4"){return(37.333*kg)}
+}
+
+
