74cc10df419cade0e0ee77d81189affd7bc1fa39
Tyler Cobian <tylercobian@169-231-157-28.wireless.ucsb.edu>
2020-02-19 10:04:20 -0800

    error checking

--- Working/Example_Function/ghg.R
+++ Working/Example_Function/ghg.R
@@ -5,6 +5,8 @@ ghg<- function(element, kg){
   else if(element == "N2O"){return(624.643*kg)}
   
   else if (element == "CH4"){return(37.333*kg)}
+  
+  else {return("Can only convert CH4, N2O, and C, in units of kg to units of kgCO2e. CH4 is measured in units of kgC, N2O in units of kgN, and C in units of kgC")}
 }
 
 
