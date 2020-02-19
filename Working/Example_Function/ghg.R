2b81ddeb8cbbc77e6c053138849e83f545ca8584
Tyler Cobian <tylercobian@169-231-157-28.wireless.ucsb.edu>
2020-02-19 11:01:49 -0800

    Updated emissions factor

--- Working/Example_Function/ghg.R
+++ Working/Example_Function/ghg.R
@@ -1,8 +1,8 @@
 ghg<- function(element, kg){
  # What you will have to put in the function, ghg("elemnt", kg = ) 
-  if(element == "C"){return(5.077*kg)} # Conversionn from kgC to kgCO2e
+  if(element == "C"){return(3.077*kg)} # Conversionn from kgC to kgCO2e
   
-  else if(element == "N2O"){return(624.643*kg)} # Conversion from kgN to KkgCO2e
+  else if(element == "N2O"){return(416.429*kg)} # Conversion from kgN to KkgCO2e
   
   else if (element == "CH4"){return(37.333*kg)} # Conversion from kgC to kgCO2e
   
